e899082701a5ddeb39ddc480edff7644e0303fff
JacobByrdCTI110 <43179092+JacobByrdCTI110@users.noreply.github.com>
2018-09-11 14:08:06 -0400

    Add files via upload
     P2T1_SalesPrediction_JacobByrd

---
+++ P2T1_SalesPrediction_JacobByrd.py
@@ -0,0 +1,15 @@
+# Calculating Sales Prediction
+# 09/11/2018
+# CTI-110 P2T1 - Sales Prediction
+# Jacob Byrd
+#
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Diaplay the profit.
+print('The profit is $', format (profit, ',.2f'))
+
